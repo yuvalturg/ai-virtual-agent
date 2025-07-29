f3618546cc8a7778255459dce684c83ecf05d2cc
Yuval Turgeman <yturgema@redhat.com>
2025-07-29 16:54:38 -0400

    wip
    Signed-off-by: Yuval Turgeman <yturgema@redhat.com>

--- backend/database.py
+++ backend/database.py
@@ -14,7 +14,7 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=False)
 
 AsyncSessionLocal = sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
